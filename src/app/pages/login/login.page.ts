7c1768e3f67b770d9e25a124dff2db1037fb498d
midas <arayamidas@gmail.com>
2024-08-31 11:45:50 -0400

    validar contraseña en login y register

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+    selector: 'app-login',
+    templateUrl: './login.page.html',
+    styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    
+    email:string = "";
+    nombre:string = "";
+    pass1:string = "";
+    
+    constructor(private router:Router, private alertController: AlertController) { }
+    
+    ngOnInit() {
+    }
+    
+    confirmarLogin() {
+        if(this.email == "" || this.nombre == "" || this.pass1 == "") {
+            this.presentAlert("Datos Inválidos", "Los datos no pueden estar vacíos.");
+            return;
+        }
+        
+        const regex = /^(?=.*\d)(?=.*[A-Z])(?=.*[\W_]).{8,}$/;
+        if(!regex.test(this.pass1)) {
+            this.presentAlert("Contraseña Incorrecta", "Intentelo de nuevo.");
+            return;
+        }
+        
+        this.router.navigate(['/home']);
+    }
+    
+    async presentAlert(title:string, msg:string, sub:string = "") {
+        const alert = await this.alertController.create({
+            header: title,
+            subHeader: sub,
+            message: msg,
+            buttons: ['OK'],
+        });
+        
+        await alert.present();
+    }
+    
 }
--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.page.html',
-  styleUrls: ['./register.page.scss'],
+    selector: 'app-register',
+    templateUrl: './register.page.html',
+    styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
+    
+    email:string = "";
+    nombre:string = "";
+    pass1:string = "";
+    pass2:string = "";
+    
+    constructor(private router:Router, private alertController: AlertController) { }
+    
+    ngOnInit() {
+    }
 
-  constructor() { }
+    confirmarRegistro() {
+        if(this.email == "" || this.nombre == "" || this.pass1 == "" || this.pass2 == "") {
+            this.presentAlert("Datos Inválidos", "Los datos no pueden estar vacíos.");
+            return;
+        }
 
-  ngOnInit() {
-  }
+        const regex = /^(?=.*\d)(?=.*[A-Z])(?=.*[\W_]).{8,}$/;
+        if(!regex.test(this.pass1)) {
+            this.presentAlert("Contraseña Inválida", "La contraseña debe tener al menos 8 carácteres, una letra mayúscula, y un símbolo.");
+            return;
+        }
 
+        if(this.pass1 != this.pass2) {
+            this.presentAlert("Contraseñas No Coinciden", "Las contraseñas no coinciden.");
+            return;
+        }
+
+        this.router.navigate(['/home']);
+    }
+    
+    async presentAlert(title:string, msg:string, sub:string = "") {
+        const alert = await this.alertController.create({
+            header: title,
+            subHeader: sub,
+            message: msg,
+            buttons: ['OK'],
+        });
+        
+        await alert.present();
+    }
+    
 }
