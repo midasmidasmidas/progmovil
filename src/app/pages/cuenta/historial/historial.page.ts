04a7577c01ac6048fa04ff07e176d45179e36c21
RodrigoRiquelmeZ <ro.riquelmez@duocuc.cl>
2024-08-30 21:14:56 -0400

    Páginas Editar cuenta y Historial de compras

--- src/app/pages/cuenta/historial/historial.page.ts
+++ src/app/pages/cuenta/historial/historial.page.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-historial',
-  templateUrl: './historial.page.html',
-  styleUrls: ['./historial.page.scss'],
+    selector: 'app-historial',
+    templateUrl: './historial.page.html',
+    styleUrls: ['./historial.page.scss'],
 })
 export class HistorialPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    
+    historial: any = [
+        {
+            imagen: "assets/img/placeholder2.jpg",
+            nombre: "Nombre 1",
+            fecha: "30/08/2024",
+            precio: 3000,
+        },
+        {
+            imagen: "assets/img/placeholder2.jpg",
+            nombre: "Nombre 2",
+            fecha: "30/08/2024",
+            precio: 8000,
+        },
+        {
+            imagen: "assets/img/placeholder2.jpg",
+            nombre: "Nombre 3",
+            fecha: "30/08/2024",
+            precio: 2000,
+        },
+    ] 
+    
+    constructor() { }
+    
+    ngOnInit() {
+    }
+    
 }
