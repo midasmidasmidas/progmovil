18bb4a19c7a8187ac830fc8e6770f736b35dd42d
midas <arayamidas@gmail.com>
2024-08-30 20:45:01 -0400

    productos en catalogo + rediseño pagina de cuenta

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,33 @@ import { Component } from '@angular/core';
     styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+    productos: any = [
+        {
+            imagen: "assets/img/placeholder.webp",
+            nombre: "Nombre 1",
+            marca: "Marca 1",
+            precio: 3000,
+        },
+        {
+            imagen: "assets/img/placeholder.webp",
+            nombre: "Nombre 2",
+            marca: "Marca 2",
+            precio: 8000,
+        },
+        {
+            imagen: "assets/img/placeholder.webp",
+            nombre: "Nombre 3",
+            marca: "Marca 3",
+            precio: 2000,
+        },
+        {
+            imagen: "assets/img/placeholder.webp",
+            nombre: "Nombre 4",
+            marca: "Marca 4",
+            precio: 6000,
+        },
+    ] 
     
     constructor() {}
     
